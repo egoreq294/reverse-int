79d43560f6261c67cfe9244584509d84e4f1b143
egoreq <egoreq@bk.ru>
2020-02-20 21:56:32 +0400

    feat: passed tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  let str='';
+  arr=String(n).split('');
+  if(arr[0]=='-'){
+    for(let i = arr.length-1; i>=1; i--){
+        str+=arr[i];
+    }
+  }
+  else{
+      for(let i = arr.length-1; i>=0; i--){
+          str+=arr[i];
+      }
+    }
+  return str;
 }
